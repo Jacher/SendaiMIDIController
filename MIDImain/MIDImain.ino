42384a7474d1d1360ea589fa79848f91c0654d23
Jacher <iangunther87@gmail.com>
2022-02-05 17:08:13 -0600

    Update MIDImain.ino
    Fleshed out new interrupt key defines and interrupt logic. Commented out old MIDI logic for the purposes of testing the new interrupt based logic.

--- MIDImain/MIDImain.ino
+++ MIDImain/MIDImain.ino
@@ -44,9 +44,35 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #define betaCHA 3
 #define betaCHB A5
 
+#define key1 5; //MIDI 60
+#define Midi1 60;
+#define key2 A0; //MIDI 61
+#define Midi2 61;
+#define key3 6; //MIDI 62
+#define Midi3 62;
+#define key4 A1; //MIDI 63
+#define Midi4 63;
+#define key5 7; //MIDI 64
+#define Midi5 64;
+#define key6 8; //MIDI 65
+#define Midi6 65;
+#define key7 A2; //MIDI 66
+#define Midi7 66;
+#define key8 9; //MIDI 67
+#define Midi8 67;
+#define key9 A3; //MIDI 68
+#define Midi9 68;
+#define key10 10; //MIDI 69
+#define Midi10 69;
+#define key11 4; //MIDI 70
+#define Midi11 70;
+#define key12 11; //MIDI 71
+#define Midi12 71;
+
 // Example for Arduino UNO with input signals on pin 2 and 3
 #define PIN_IN1 A5
 #define PIN_IN2 3
+
 
 #define midiLED 13 //LED at Pin 13 for MIDI signal indicator
 
@@ -57,7 +83,7 @@ uint8_t blackNum = 0;
 uint8_t encAlpha = 0;
 uint8_t encBeta = 0;
 
-bool pinState[16];
+bool pinState[16]=1;
 /*
 bool pinState2=1;
 bool pinState3=1;
@@ -104,37 +130,139 @@ void pciSetup(byte pin){
 }
 
 //D8-D13 Interrupt Handler
+//Keys 6,8,10,12
 ISR(PCINT0_vect){
-  for(byte i=8;i++;i<12){
-  if(digitalRead(i)==0 && pinState[i]==1){
+  if(digitalRead(key6)==0 && pinState[6]==1){
     digitalWrite(midiLED,HIGH);     // Blink the midiLED
-    MIDI.sendNoteOn(i+65,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
-    pinState[i]=0;
+    MIDI.sendNoteOn(Midi6,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[6]=0;
   }
-  elseif(digitalRead(i)==1 && pinState[i]==0){
-    MIDI.sendNoteOff(i+65,0,1);   // Stop the note
+  else if(digitalRead(key6)==1 && pinState[6]==0){
+    MIDI.sendNoteOff(Midi6,0,1);   // Stop the note
     digitalWrite(midiLED,LOW);
-    pinState[i]=1;
-    }
+    pinState[6]=1;
+  }
+  if(digitalRead(key8)==0 && pinState[8]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi8,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[8]=0;
+  }
+  else if(digitalRead(key8)==1 && pinState[8]==0){
+    MIDI.sendNoteOff(Midi8,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[8]=1;
+  }
+  if(digitalRead(key10)==0 && pinState[10]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi10,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[10]=0;
+  }
+  else if(digitalRead(key10)==1 && pinState[10]==0){
+    MIDI.sendNoteOff(Midi10,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[10]=1;
+  }
+  if(digitalRead(key12)==0 && pinState[12]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi12,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[12]=0;
+  }
+  else if(digitalRead(key12)==1 && pinState[12]==0){
+    MIDI.sendNoteOff(Midi12,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[12]=1;
   }
 }
 
 //A0-A5 Interrupt Handler
+//Keys 2,4,7,9
 ISR(PCINT1_vect){
-  for(byte i=0;i++;i<4){
-  if(digitalRead(i)==0 && pinState[i]==1){
+  if(digitalRead(key2)==0 && pinState[2]==1){
     digitalWrite(midiLED,HIGH);     // Blink the midiLED
-    MIDI.sendNoteOn(i+65,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
-    pinState[i]=0;
+    MIDI.sendNoteOn(Midi2,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[2]=0;
   }
-  elseif(digitalRead(i)==1 && pinState[i]==0){
-    MIDI.sendNoteOff(i+65,0,1);   // Stop the note
+  else if(digitalRead(key2)==1 && pinState[2]==0){
+    MIDI.sendNoteOff(Midi2,0,1);   // Stop the note
     digitalWrite(midiLED,LOW);
-    pinState[i]=1;
-    }
+    pinState[2]=1;
+  }
+  if(digitalRead(key4)==0 && pinState[4]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi4,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[4]=0;
+  }
+  else if(digitalRead(key4)==1 && pinState[4]==0){
+    MIDI.sendNoteOff(Midi4,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[4]=1;
+  }
+  if(digitalRead(key7)==0 && pinState[7]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi7,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[7]=0;
+  }
+  else if(digitalRead(key7)==1 && pinState[7]==0){
+    MIDI.sendNoteOff(Midi7,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[7]=1;
+  }
+  if(digitalRead(key9)==0 && pinState[9]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi9,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[9]=0;
+  }
+  else if(digitalRead(key9)==1 && pinState[9]==0){
+    MIDI.sendNoteOff(Midi9,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[9]=1;
   }
 }
 
+//D0-D7 Interrupt Handler
+//Keys 1,3,5,11
+ISR(PCINT2_vect){
+  if(digitalRead(key1)==0 && pinState[1]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi1,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[1]=0;
+  }
+  else if(digitalRead(key1)==1 && pinState[1]==0){
+    MIDI.sendNoteOff(Midi1,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[1]=1;
+  }
+  if(digitalRead(key3)==0 && pinState[3]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi3,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[3]=0;
+  }
+  else if(digitalRead(key3)==1 && pinState[3]==0){
+    MIDI.sendNoteOff(Midi3,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[3]=1;
+  }
+  if(digitalRead(key5)==0 && pinState[5]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi5,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[5]=0;
+  }
+  else if(digitalRead(key5)==1 && pinState[5]==0){
+    MIDI.sendNoteOff(Midi5,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[5]=1;
+  }
+  if(digitalRead(key11)==0 && pinState[11]==1){
+    digitalWrite(midiLED,HIGH);     // Blink the midiLED
+    MIDI.sendNoteOn(Midi11,127,1);  // Send a Note (pitch 65, velo 127 on channel 1)
+    pinState[11]=0;
+  }
+  else if(digitalRead(key11)==1 && pinState[11]==0){
+    MIDI.sendNoteOff(Midi11,0,1);   // Stop the note
+    digitalWrite(midiLED,LOW);
+    pinState[11]=1;
+  }
+}
 
 
 //**GENERAL SETUP**//
@@ -228,6 +356,7 @@ void loop() {
   delay(500); //Delay so serial monitor is not spammed with input responses
   }
 */
+/* Commented out for new interrupt functionality
   if(blackNum == 1){
     digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(42,127,1);  // Send a Note (pitch 42, velo 127 on channel 1)
@@ -263,4 +392,5 @@ void loop() {
     MIDI.sendNoteOff(46,0,1);   // Stop the note
     digitalWrite(midiLED,LOW);
   }
+*/
 }
