9131f0f9680d12952977f15871cdbe7d4dc2b05b
Jacher <iangunther87@gmail.com>
2022-05-19 16:48:17 -0500

    6 Knob Expansion Post-Test
    Tested the changes made to adapt the code to the final control scheme for the showcase prototype. l Midi messages sent as expected.

--- MIDImain/MIDImain.ino
+++ MIDImain/MIDImain.ino
@@ -35,111 +35,115 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 //**PORT <-> I/O DEFINITIONS**//
 //Encoders
-#define alphaCHA 2
-#define alphaCHB A4
-#define betaCHA 3
-#define betaCHB A5
-/* 6 Encoder SETUP
-#define chiCHA 2
-#define chiCHB A4
-#define deltaCHA 3
-#define deltaCHB A5
-#define epsilonCHA 2
-#define epsilonCHB A4
-#define phiCHA 3
-#define phiCHB A5
-*/
+//Alpha Encoder (Main Cursor)
+#define alphaCHA 22
+#define alphaCHB 24
+#define alphaSW 26
+#define MidiSel 28 //Select/OK
+//Beta Encoder (Program Change)
+#define betaCHA 28
+#define betaCHB 30
+#define betaSW 32
+#define MidiSave 26 //Save Program
+// 6 Encoder SETUP
+//Chi Encoder (Grain Duration)
+#define chiCHA 34
+#define chiCHB 36
+#define chiSW 38
+//#define MidiCSW (unused)
+//Delta Encoder (Number of Grains)
+#define deltaCHA 40
+#define deltaCHB 42
+//Epsilon Encoder (Spray)
+#define epsilonCHA 44
+#define epsilonCHB 46
+//Phi Encoder (Volume)
+#define phiCHA 48
+#define phiCHB 50
+
 
 //Keys
-#define key1 5 //MIDI 60
+#define key1 2 //MIDI 60
 #define Midi1 60
-#define key2 A0 //MIDI 61
+#define key2 3 //MIDI 61
 #define Midi2 61
-#define key3 6 //MIDI 62
+#define key3 4 //MIDI 62
 #define Midi3 62
-#define key4 A1 //MIDI 63
+#define key4 5 //MIDI 63
 #define Midi4 63
-#define key5 7 //MIDI 64
+#define key5 6 //MIDI 64
 #define Midi5 64
-#define key6 8 //MIDI 65
+#define key6 7 //MIDI 65
 #define Midi6 65
-#define key7 A2 //MIDI 66
+#define key7 8 //MIDI 66
 #define Midi7 66
 #define key8 9 //MIDI 67
 #define Midi8 67
-#define key9 A3 //MIDI 68
+#define key9 10 //MIDI 68
 #define Midi9 68
-#define key10 10 //MIDI 69
+#define key10 11 //MIDI 69
 #define Midi10 69
-#define key11 4 //MIDI 70
+#define key11 12 //MIDI 70
 #define Midi11 70
-#define key12 11 //MIDI 71
+#define key12 13 //MIDI 71
 #define Midi12 71
+#define Rec 52 //Record button
+#define MidiRec 25 //CC 25 EV 1
 
-//Lights
-#define midiLED 13 //LED at Pin 13 for MIDI signal indicator
 
 //**DECLARATIONS**//
-volatile bool pinState[16]={1};//for state ISR state monitoring
+volatile bool pinState[16]={1};//for pin state monitoring
 
 // Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
 RotaryEncoder encoderA(alphaCHB, alphaCHA, RotaryEncoder::LatchMode::TWO03);
 RotaryEncoder encoderB(betaCHB, betaCHA, RotaryEncoder::LatchMode::TWO03);
-/* 6 Encoder SETUP
 RotaryEncoder encoderC(chiCHB, chiCHA, RotaryEncoder::LatchMode::TWO03);
 RotaryEncoder encoderD(deltaCHB, deltaCHA, RotaryEncoder::LatchMode::TWO03);
 RotaryEncoder encoderE(epsilonCHB, epsilonCHA, RotaryEncoder::LatchMode::TWO03);
 RotaryEncoder encoderF(phiCHB, phiCHA, RotaryEncoder::LatchMode::TWO03);
-*/
 
 //**PCINT SETUP**//
+/*
 void pciSetup(byte pin){
   *digitalPinToPCMSK(pin)|= bit(digitalPinToPCMSKbit(pin));
   PCIFR |= bit(digitalPinToPCICRbit(pin));
   PCICR |= bit(digitalPinToPCICRbit(pin));
 }
-
+*/
+/*
 //D8-D13 Interrupt Handler
 //Keys 6,8,10,12
 ISR(PCINT0_vect){
   if(digitalRead(key6)==0 && pinState[6]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi6,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[6]=0;
   }
   else if(digitalRead(key6)==1 && pinState[6]==0){
     MIDI.sendNoteOff(Midi6,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[6]=1;
   }
   if(digitalRead(key8)==0 && pinState[8]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi8,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[8]=0;
   }
   else if(digitalRead(key8)==1 && pinState[8]==0){
     MIDI.sendNoteOff(Midi8,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[8]=1;
   }
   if(digitalRead(key10)==0 && pinState[10]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi10,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[10]=0;
   }
   else if(digitalRead(key10)==1 && pinState[10]==0){
     MIDI.sendNoteOff(Midi10,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[10]=1;
   }
   if(digitalRead(key12)==0 && pinState[12]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi12,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[12]=0;
   }
   else if(digitalRead(key12)==1 && pinState[12]==0){
     MIDI.sendNoteOff(Midi12,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[12]=1;
   }
 }
@@ -148,43 +152,35 @@ ISR(PCINT0_vect){
 //Keys 2,4,7,9
 ISR(PCINT1_vect){
   if(digitalRead(key2)==0 && pinState[2]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi2,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[2]=0;
   }
   else if(digitalRead(key2)==1 && pinState[2]==0){
     MIDI.sendNoteOff(Midi2,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[2]=1;
   }
   if(digitalRead(key4)==0 && pinState[4]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi4,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[4]=0;
   }
   else if(digitalRead(key4)==1 && pinState[4]==0){
     MIDI.sendNoteOff(Midi4,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[4]=1;
   }
   if(digitalRead(key7)==0 && pinState[7]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi7,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[7]=0;
   }
   else if(digitalRead(key7)==1 && pinState[7]==0){
     MIDI.sendNoteOff(Midi7,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[7]=1;
   }
   if(digitalRead(key9)==0 && pinState[9]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi9,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[9]=0;
   }
   else if(digitalRead(key9)==1 && pinState[9]==0){
     MIDI.sendNoteOff(Midi9,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[9]=1;
   }
 }
@@ -194,83 +190,65 @@ ISR(PCINT1_vect){
 //Keys 1,3,5,11
 ISR(PCINT2_vect){
   if(digitalRead(key1)==0 && pinState[1]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi1,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[1]=0;
   }
   else if(digitalRead(key1)==1 && pinState[1]==0){
     MIDI.sendNoteOff(Midi1,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[1]=1;
   }
   if(digitalRead(key3)==0 && pinState[3]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi3,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[3]=0;
   }
   else if(digitalRead(key3)==1 && pinState[3]==0){
     MIDI.sendNoteOff(Midi3,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[3]=1;
   }
   if(digitalRead(key5)==0 && pinState[5]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi5,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[5]=0;
   }
   else if(digitalRead(key5)==1 && pinState[5]==0){
     MIDI.sendNoteOff(Midi5,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[5]=1;
   }
   if(digitalRead(key11)==0 && pinState[11]==1){
-    digitalWrite(midiLED,HIGH);     // Blink the midiLED
     MIDI.sendNoteOn(Midi11,127,1);  // Send a Note (pitch,velocity,channel)
     pinState[11]=0;
   }
   else if(digitalRead(key11)==1 && pinState[11]==0){
     MIDI.sendNoteOff(Midi11,0,1);   // Stop the note
-    digitalWrite(midiLED,LOW);
     pinState[11]=1;
   }
 }
-
+*/
 
 //**GENERAL SETUP**//
 void setup() {
-  pinMode(midiLED, OUTPUT);
   MIDI.begin(4); // Launch MIDI with default options
 
   Serial.begin(115200); //sets baud rate for serial monitor and MIDI signalling
 
   //Input settings for keyboard keys in pullup resistor configuration
-  //Black Keys
-  pinMode(A0, INPUT_PULLUP);
-  pciSetup(A0);
-  pinMode(A1, INPUT_PULLUP);
-  pciSetup(A1);
-  pinMode(A2, INPUT_PULLUP);
-  pciSetup(A2);
-  pinMode(A3, INPUT_PULLUP);
-  pciSetup(A3);
-  pinMode(4, INPUT_PULLUP);
-  pciSetup(4);
-  //White Keys
-  pinMode(5, INPUT_PULLUP);
-  pciSetup(5);
-  pinMode(6, INPUT_PULLUP);
-  pciSetup(6);
-  pinMode(7, INPUT_PULLUP);
-  pciSetup(7);
-  pinMode(8, INPUT_PULLUP);
-  pciSetup(8);
-  pinMode(9, INPUT_PULLUP);
-  pciSetup(9);
-  pinMode(10, INPUT_PULLUP);
-  pciSetup(10);
-  pinMode(11, INPUT_PULLUP);
-  pciSetup(11);
-
+  pinMode(key1, INPUT_PULLUP);
+  pinMode(key2, INPUT_PULLUP);
+  pinMode(key3, INPUT_PULLUP);
+  pinMode(key4, INPUT_PULLUP);
+  pinMode(key5, INPUT_PULLUP);
+  pinMode(key6, INPUT_PULLUP);
+  pinMode(key7, INPUT_PULLUP);
+  pinMode(key8, INPUT_PULLUP);
+  pinMode(key9, INPUT_PULLUP);
+  pinMode(key10, INPUT_PULLUP);
+  pinMode(key11, INPUT_PULLUP);
+  pinMode(key12, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(26, INPUT_PULLUP);
+  pinMode(32, INPUT_PULLUP);
+  pinMode(52, INPUT_PULLUP);
+  
   //Rotary Encoder input settings
   //encAlpha
   pinMode(alphaCHA, INPUT);
@@ -282,7 +260,7 @@ void setup() {
   pinMode(betaCHB, INPUT);
   digitalWrite(betaCHA, HIGH);
   digitalWrite(betaCHB, HIGH);
-  /* 6 Encoder SETUP
+  // 6 Encoder SETUP
   //encChi
   pinMode(chiCHA, INPUT);
   pinMode(chiCHB, INPUT);
@@ -303,40 +281,207 @@ void setup() {
   pinMode(phiCHB, INPUT);
   digitalWrite(phiCHA, HIGH);
   digitalWrite(phiCHB, HIGH);
-  */
 }
 
 
 //**MAIN LOOP**//
 void loop() {
   encoderA.tick(); //Update encoder position
-  switch(encoderA.getDirection())
+  switch(encoderA.getDirection()) //**MAIN CURSOR**
   {
     case RotaryEncoder::Direction::NOROTATION:
-      digitalWrite(midiLED,LOW);     // Blink the midiLED
       break;
     case RotaryEncoder::Direction::CLOCKWISE:
-      digitalWrite(midiLED,HIGH);     // Blink the midiLED
-      MIDI.sendControlChange(21,1,1);
+      MIDI.sendControlChange(27,1,1); //(CC,Value,Channel)
       break;
     case RotaryEncoder::Direction::COUNTERCLOCKWISE:
-      digitalWrite(midiLED,HIGH);     // Blink the midiLED
-      MIDI.sendControlChange(21,127,1);
+      MIDI.sendControlChange(27,127,1);
       break;
   }
   encoderB.tick();//Update encoder position
-  switch(encoderB.getDirection())
+  switch(encoderB.getDirection()) //**PROGRAM CHANGE**
   {
     case RotaryEncoder::Direction::NOROTATION:
-      digitalWrite(midiLED,LOW);     // Blink the midiLED
       break;
     case RotaryEncoder::Direction::CLOCKWISE:
-      digitalWrite(midiLED,HIGH);     // Blink the midiLED
-      MIDI.sendControlChange(20,1,1);
+      MIDI.sendControlChange(24,1,1);
       break;
     case RotaryEncoder::Direction::COUNTERCLOCKWISE:
-      digitalWrite(midiLED,HIGH);     // Blink the midiLED
-      MIDI.sendControlChange(20,127,1);
+      MIDI.sendControlChange(24,127,1);
       break;
+  }
+  encoderC.tick();//Update encoder position
+  switch(encoderC.getDirection()) //**GRAIN DURATION**
+  {
+    case RotaryEncoder::Direction::NOROTATION:
+      break;
+    case RotaryEncoder::Direction::CLOCKWISE:
+      MIDI.sendControlChange(21,1,1);
+      break;
+    case RotaryEncoder::Direction::COUNTERCLOCKWISE:
+      MIDI.sendControlChange(21,127,1);
+      break;
+  }
+  encoderD.tick();//Update encoder position
+  switch(encoderD.getDirection()) //**NUMBER OF GRAINS**
+  {
+    case RotaryEncoder::Direction::NOROTATION:
+      break;
+    case RotaryEncoder::Direction::CLOCKWISE:
+      MIDI.sendControlChange(23,1,1);
+      break;
+    case RotaryEncoder::Direction::COUNTERCLOCKWISE:
+      MIDI.sendControlChange(23,127,1);
+      break;
+  }
+  encoderE.tick();//Update encoder position
+  switch(encoderE.getDirection()) //**SPRAY**
+  {
+    case RotaryEncoder::Direction::NOROTATION:
+      break;
+    case RotaryEncoder::Direction::CLOCKWISE:
+      MIDI.sendControlChange(22,1,1);
+      break;
+    case RotaryEncoder::Direction::COUNTERCLOCKWISE:
+      MIDI.sendControlChange(22,127,1);
+      break;
+  }
+  encoderF.tick();//Update encoder position
+  switch(encoderF.getDirection()) //**VOLUME**
+  {
+    case RotaryEncoder::Direction::NOROTATION:
+      break;
+    case RotaryEncoder::Direction::CLOCKWISE:
+      MIDI.sendControlChange(7,1,1);
+      break;
+    case RotaryEncoder::Direction::COUNTERCLOCKWISE:
+      MIDI.sendControlChange(7,127,1);
+      break;
+  }
+//Record, Select/OK, Program Save button functionality
+
+//**RECORD**
+  if(digitalRead(Rec)==0 && pinState[13]==1){
+    MIDI.sendNoteOn(MidiRec,1,1);  // Send a Note (pitch,velocity,channel)
+    pinState[13]=0;
+  }
+  else if(digitalRead(Rec)==1 && pinState[13]==0){
+    pinState[13]=1;
+  }
+//**SELECT/OK
+  if(digitalRead(alphaSW)==0 && pinState[14]==1){
+    MIDI.sendNoteOn(MidiSel,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[14]=0;
+  }
+  else if(digitalRead(alphaSW)==1 && pinState[14]==0){
+    MIDI.sendNoteOff(MidiSel,0,1);   // Stop the note
+    pinState[14]=1;
+  }
+//**SAVE**
+  if(digitalRead(betaSW)==0 && pinState[15]==1){
+    MIDI.sendNoteOn(MidiSave,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[15]=0;
+  }
+  else if(digitalRead(betaSW)==1 && pinState[15]==0){
+    MIDI.sendNoteOff(MidiSave,0,1);   // Stop the note
+    pinState[15]=1;
+  }
+
+//PIANO KEYS
+  if(digitalRead(key1)==0 && pinState[1]==1){
+    MIDI.sendNoteOn(Midi1,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[1]=0;
+  }
+  else if(digitalRead(key1)==1 && pinState[1]==0){
+    MIDI.sendNoteOff(Midi1,0,1);   // Stop the note
+    pinState[1]=1;
+  }
+  if(digitalRead(key2)==0 && pinState[2]==1){
+    MIDI.sendNoteOn(Midi2,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[2]=0;
+  }
+  else if(digitalRead(key2)==1 && pinState[2]==0){
+    MIDI.sendNoteOff(Midi2,0,1);   // Stop the note
+    pinState[2]=1;
+  }
+  if(digitalRead(key3)==0 && pinState[3]==1){
+    MIDI.sendNoteOn(Midi3,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[3]=0;
+  }
+  else if(digitalRead(key3)==1 && pinState[3]==0){
+    MIDI.sendNoteOff(Midi3,0,1);   // Stop the note
+    pinState[3]=1;
+  }
+  if(digitalRead(key4)==0 && pinState[4]==1){
+    MIDI.sendNoteOn(Midi4,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[4]=0;
+  }
+  else if(digitalRead(key4)==1 && pinState[4]==0){
+    MIDI.sendNoteOff(Midi4,0,1);   // Stop the note
+    pinState[4]=1;
+  }
+  if(digitalRead(key5)==0 && pinState[5]==1){
+    MIDI.sendNoteOn(Midi5,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[5]=0;
+  }
+  else if(digitalRead(key5)==1 && pinState[5]==0){
+    MIDI.sendNoteOff(Midi5,0,1);   // Stop the note
+    pinState[5]=1;
+  }
+  if(digitalRead(key6)==0 && pinState[6]==1){
+    MIDI.sendNoteOn(Midi6,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[6]=0;
+  }
+  else if(digitalRead(key6)==1 && pinState[6]==0){
+    MIDI.sendNoteOff(Midi6,0,1);   // Stop the note
+    pinState[6]=1;
+  }
+  if(digitalRead(key7)==0 && pinState[7]==1){
+    MIDI.sendNoteOn(Midi7,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[7]=0;
+  }
+  else if(digitalRead(key7)==1 && pinState[7]==0){
+    MIDI.sendNoteOff(Midi7,0,1);   // Stop the note
+    pinState[7]=1;
+  }
+  if(digitalRead(key8)==0 && pinState[8]==1){
+    MIDI.sendNoteOn(Midi8,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[8]=0;
+  }
+  else if(digitalRead(key8)==1 && pinState[8]==0){
+    MIDI.sendNoteOff(Midi8,0,1);   // Stop the note
+    pinState[8]=1;
+  }
+  if(digitalRead(key9)==0 && pinState[9]==1){
+    MIDI.sendNoteOn(Midi9,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[9]=0;
+  }
+  else if(digitalRead(key9)==1 && pinState[9]==0){
+    MIDI.sendNoteOff(Midi9,0,1);   // Stop the note
+    pinState[9]=1;
+  }
+  if(digitalRead(key10)==0 && pinState[10]==1){
+    MIDI.sendNoteOn(Midi10,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[10]=0;
+  }
+  else if(digitalRead(key10)==1 && pinState[10]==0){
+    MIDI.sendNoteOff(Midi10,0,1);   // Stop the note
+    pinState[10]=1;
+  }
+  if(digitalRead(key11)==0 && pinState[11]==1){
+    MIDI.sendNoteOn(Midi11,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[11]=0;
+  }
+  else if(digitalRead(key11)==1 && pinState[11]==0){
+    MIDI.sendNoteOff(Midi11,0,1);   // Stop the note
+    pinState[11]=1;
+  }
+  if(digitalRead(key12)==0 && pinState[12]==1){
+    MIDI.sendNoteOn(Midi12,127,1);  // Send a Note (pitch,velocity,channel)
+    pinState[12]=0;
+  }
+  else if(digitalRead(key12)==1 && pinState[12]==0){
+    MIDI.sendNoteOff(Midi12,0,1);   // Stop the note
+    pinState[12]=1;
   }
 }
